54b07ebdf0d8ab2033b212794580dc271b6ca590
Ditronian <davidgereau@gmail.com>
2020-02-14 17:56:32 -0700

    Added ASP.Net Project for Web Server with the beginings of DatabaseConnection code.

---
+++ Server/App_Code/Database/DatabaseConnection.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+//Creates a connection to the sql server, which is opened and closed as an intermediary between specific table methods and the database.
+public class DatabaseConnection
+{
+    SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ToString());
+
+    //Used for Insert, Update, and Delete commands
+    public void uploadCommand(string query, SqlParameter[] parameters = null)
+    {
+        //Open Connection
+        sqlConnection.Open();
+
+        //Create Command
+        SqlCommand cmd = new SqlCommand(query);
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.Connection = sqlConnection;
+        if (parameters != null) cmd.Parameters.AddRange(parameters);
+
+        //Execute command on database
+        cmd.ExecuteNonQuery();
+
+        //Close Connection
+        sqlConnection.Close();
+    }
+
+    //Used for Insert, Update, and Delete commands that return the id of the item modified
+    public int uploadAndReturnCommand(string query, SqlParameter outPutVal, SqlParameter[] parameters = null)
+    {
+        //Open Connection
+        sqlConnection.Open();
+
+        int outputID = -1971;
+
+        //Create Command
+        SqlCommand cmd = new SqlCommand(query);
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.Connection = sqlConnection;
+        if (parameters != null) cmd.Parameters.AddRange(parameters);
+        cmd.Parameters.Add(outPutVal);
+
+        //Execute command on database
+        cmd.ExecuteNonQuery();
+
+        //Close Connection
+        sqlConnection.Close();
+
+        if (outPutVal.Value != DBNull.Value) outputID = Convert.ToInt32(outPutVal.Value);
+        return outputID;
+    }
+
+    //Used for SELECT commands
+    public DataSet downloadCommand(string query, SqlParameter[] parameters = null)
+    {
+        //Open Connection
+        sqlConnection.Open();
+
+        //Create Command
+        SqlCommand cmd = new SqlCommand(query);
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.Connection = sqlConnection;
+        if (parameters != null) cmd.Parameters.AddRange(parameters);
+
+        //Recieve DataSet
+        SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+        DataSet data = new DataSet();
+        adapter.Fill(data);
+
+        //Close Connection and return dataSet
+        sqlConnection.Close();
+        return data;
+    }
+}
---
+++ Server/Server.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace InnovateServer
+{
+    public partial class Server : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // Some example stuff I will try to link into JS...
+        //[WebMethod]
+        //public static RandoClass testMe(string name, int age, char gender)
+        //{
+        //    return new RandoClass("Bob", 9000, 'M');
+        //}
+    }
+}
